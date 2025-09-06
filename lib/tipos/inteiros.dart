c6d05dfca38d46ca1566fee46697e3db47f11278
GabrielRuanDias <gabrielruandias12345@gmail.com>
2025-09-06 10:40:21 -0300

    Aula de inteiros

---
+++ lib/tipos/inteiros.dart
@@ -0,0 +1,5 @@
+void main() {
+  int altura = 188;
+
+  print("Altura: $altura cm");
+}
---
+++ lib/tipos/variaveis.dart
@@ -0,0 +1,20 @@
+void main() {
+  var nome = "Gabriel";
+
+  nome = "Gabriel raun";
+
+  var idade = 11;
+
+  var peso = 100.89;
+
+  peso = 88;
+
+  var maiorIdade = true;
+
+  const nomeCompleto = "Gabriel Ruan Dias Martires";
+
+  final int altura;
+
+  late String nomePai;
+  nomePai = 'adadsa';
+}
